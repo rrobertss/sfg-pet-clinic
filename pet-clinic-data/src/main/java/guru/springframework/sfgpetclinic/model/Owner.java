d30e61ee46599a4f4d1fbb9bf27b7991cb47e081
Robert Siwak <rsiwak@gmail.com>
2019-02-05 22:09:05 +0100

    Add contact into to Owner, create PetType map service impl. Closes #28, closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -17,19 +17,38 @@ public class Owner extends Person{
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet>pets;
-	/**
-	 * @return the pets
-	 */
+	
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 	public Set<Pet> getPets() {
 		return pets;
 	}
-	/**
-	 * @param pets the pets to set
-	 */
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
+	
 	
 	
 	
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,18 @@
+/*
+ *  Company: RS
+ *  Project: pet-clinic-data
+ *  Created: 5 lut 2019  21:59:29
+ *  Author:  RS
+ */
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ *<p></p><br>5 lut 2019
+ * @author RS
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
