fe305eabd457c3454d1080c1c47724a1e5357072
Robert Siwak <rsiwak@gmail.com>
2019-02-12 22:06:24 +0100

    Add JPA adnotations to BaseEntity. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -8,16 +8,24 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  *<p></p><br>3 sty 2019
  * @author RS
  *
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
